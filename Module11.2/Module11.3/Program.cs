e9749e764e339c93ebdcbf93f01e0152ac43d98e
vypopov <popovvladimir23@gmail.com>
2020-10-26 21:10:54 +0300

    Задание 11.3.2

---
+++ Module11.2/Module11.3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Module11._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyName = "Jane";
+            byte MyAge = 27;
+            bool HaveIApet = true;
+            double MyShoeSize = 37.5;
+
+            Console.WriteLine("My name is " + MyName);
+            Console.WriteLine("My age id " + MyAge);
+            Console.WriteLine("Do I have a pet? " + HaveIApet);
+            Console.WriteLine("My shoe size is " + MyShoeSize);
+
+            Console.ReadKey();
+
+        }
+    }
+}
