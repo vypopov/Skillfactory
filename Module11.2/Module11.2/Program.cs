956dd354a5f831473392fccea83a9751b3fd196e
vypopov <popovvladimir23@gmail.com>
2020-10-26 20:46:28 +0300

    Задание 11.2.3

---
+++ Module11.2/Module11.2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Module11._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyName = "Jane";
+            Console.WriteLine(MyName);
+            Console.ReadKey();
+        }
+    }
+}
