bf3d102c3ea82bd82fe60881dc5b7e501cfb23ff
vypopov <popovvladimir23@gmail.com>
2020-11-03 12:48:53 +0300

    Добавил финальное задание 11 модуля

---
+++ Module11.2/FinalTask_module_11.7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace FinalTask_module_11._7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                Console.Write("Enter your name: ");
+                string name = Console.ReadLine();
+                Console.Write("Enter your age: ");
+                byte age = checked((byte)int.Parse(Console.ReadLine()));
+                Console.WriteLine("Your name is {0} and age is {1}", name, age);
+                Console.Write("Enter your birthdate: ");
+                string birthdate = Console.ReadLine();
+                Console.WriteLine("Your birthdate is {0}", birthdate);
+                Console.ReadKey();
+           
+        }
+    }
+
+}
