bba180f5173911cd01ba019e0862360d6ae1c1ae
vypopov <popovvladimir23@gmail.com>
2020-11-19 21:22:51 +0300

    Итоговый проект модуля 13

---
+++ Module13/Module13/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Module13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        static (string Name, string LastName, byte Age, string HasPet) GetUserData()
+        {
+            (string Name, string LastName, byte Age, string HasPet) UserData;
+            UserData.Age = 1;
+            UserData.Name = "ss";
+            UserData.LastName = "w";
+            UserData.HasPet = "Да";
+            return UserData;
+        }
+    }   
+}
