c506620e958ee184e111bc95b6593f017e8f21ae
vypopov <popovvladimir23@gmail.com>
2020-12-03 23:19:13 +0300

    Финальное задание 15 модуля

---
+++ Module15/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    abstract class Delivery 
+    { 
+    
+    }
+}
