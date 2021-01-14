3b11c028197916f0137cd77bae58dfec60ff4499
vypopov <popovvladimir23@gmail.com>
2021-01-14 22:12:32 +0300

    Задание 20 модуля

--- Module15/Program.cs
+++ Module15/Program.cs
@@ -1,20 +1,26 @@
-﻿using System;
-
-namespace Module15
+﻿namespace Module15
 {
     class Program
     {
         static void Main(string[] args)
         {
+            //
+
         }
     }
-    abstract class Delivery 
+    abstract class Delivery
     {
-       
+        public string Addres;
+    }
+    class PickPointDelivery : Delivery
+    {
+        private string PickPointAddres;
+        private int CageCode;
     }
     public class Order
     {
         public string DeliveryAdress;
-        
+
     }
+
 }
---
+++ Module20/Ex20_1_5.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module20
+{
+    class Ex20_1_5
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
