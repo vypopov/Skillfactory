8ab4e5f38bac20ff53dcd6ec6f4cff4f2290f044
vypopov <popovvladimir23@gmail.com>
2021-01-20 21:19:54 +0300

    Финальные задания 21 модуля

---
+++ Module21/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CountWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.ReadLine();
+        }
+    }
+}
